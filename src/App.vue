36c561cfe45f756c6ad1681aacba88ea936b7f4e
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-07-11 18:14:53 -0300

    :art: Criando cabeçalho da página

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import { RouterLink, RouterView } from 'vue-router' 
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
+import TheHeader from '@/components/layout/TheNavbar.vue'
+
 
 // initialize components based on data attribute selectors
 onMounted(() => {
@@ -10,7 +12,7 @@ onMounted(() => {
 </script>
 
 <template>
-  
+  <TheHeader />
   <RouterView />
 </template>
 

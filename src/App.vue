fdf47f5b8daed3784c78c89eeaf0d78f583311d7
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-07-12 16:54:58 -0300

    :art: Criado footer da página

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import { onMounted } from 'vue'
 import { initFlowbite } from 'flowbite'
 import TheHeader from '@/components/layout/TheNavbar.vue'
-
+import TheFooter from './components/layout/TheFooter.vue'
 
 // initialize components based on data attribute selectors
 onMounted(() => {
@@ -13,9 +13,16 @@ onMounted(() => {
 
 <template>
   <TheHeader />
-  <RouterView />
+  <main class="conteudo_main bg-slate-100 dark:bg-gray-950">
+    <RouterView />
+  </main>
+  <footer class="bg-slate-100 dark:bg-gray-950 p-1">
+    <TheFooter />
+  </footer>
 </template>
 
 <style scoped>
-
+.conteudo_main{
+  min-height: calc(100vh - 85px);
+}
 </style>

b96c30177148e5f631b4c412d9d397539fbfa361
Rhuan Guilherme de Sousa Silva <rhuan.g.silva@stf.jus.br>
2023-07-26 13:50:39 -0300

    Ajustando icone de requisição :art:

--- src/components/layout/AreaCards.vue
+++ src/components/layout/AreaCards.vue
@@ -14,15 +14,19 @@ const chamados = useRegistrosStore()
                 <button @click="chamado.concluido = false" v-if="chamado.concluido" class="absolute inline-flex items-center justify-center w-6 h-6 font-bold text-white hover:bg-red-500 bg-green-500 border-2 border-white rounded-md -top-2 -right-2 dark:border-gray-900"><span class="material-symbols-outlined">done</span></button>
                 <button v-if="!chamado.concluido" @click="chamados.marcarConcluido(index)" class="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold hover:bg-green-500 dark:text-white dark:border-gray-300 border-gray-500 text-black border rounded-md top-1 right-2 "><span class="material-symbols-outlined text-sm">done</span></button>
                 <button v-if="!chamado.concluido" @click="chamados.removerChamado(index)" class="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold hover:bg-red-700 dark:text-white dark:border-gray-300 border-gray-500 text-black border rounded-md top-1 right-9 "><span class="material-symbols-outlined text-sm">delete</span></button>
-                <div class="flex items-center justify-center gap-3 text-lg font-semibold">
-                    <img v-if="chamado.checkTipo == 'requisicao'" src="@/assets/img/requisicaoIcon.png" alt="">
-                    <img v-if="chamado.checkTipo == 'incidente'" src="@/assets/img/incidenteIcon.png" alt="">
-                    <p class="mt-3">{{ chamado.nome }} - {{ chamado.ramal }}</p>
+                <div class="flex items-center justify-center gap-3 text-lg font-semibold mt-3">
+                    <div v-if="chamado.checkTipo == 'requisicao'">
+                        <img  src="@/assets/img/requisicaoIcon.png" alt="">
+                    </div>
+                    <div v-if="chamado.checkTipo == 'incidente'">
+                        <img  src="@/assets/img/incidenteIcon.png" alt="">
+                    </div>
+                    <div >{{ chamado.nome }} - {{ chamado.ramal }}</div>
                 </div>
                 <div class="w-full h-[1px] bg-slate-400"></div>
                 <div class="flex flex-col gap-2 mt-3">
         
-                    <p v-if="chamado.checkTipo === 'requisicao'">Sr(a). {{ chamado.nome }} entrou em contato solicitando {{ chamado.informacao }}</p>
+                    <p v-if="chamado.checkTipo === 'requisicao'">Prezado(a) Sr(a). {{ chamado.nome }} entrou em contato solicitando {{ chamado.informacao }}</p>
                     <p v-else>Prezado(a) Sr(a). {{ chamado.nome }} entrou em contato informando {{ chamado.informacao }}</p>
                     <p>Nome: {{ chamado.nome }}</p>
                     <p>Login: {{ chamado.login }}</p>

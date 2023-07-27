ba0498cdfe465badf81aa820de6ef6b4e74f752c
Rhuan Guilherme de Sousa Silva <rhuan.g.silva@stf.jus.br>
2023-07-27 11:30:59 -0300

    :art: Atualizando modal da edição de chamados

--- src/components/layout/AreaCards.vue
+++ src/components/layout/AreaCards.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRegistrosStore } from '@/stores/registros'
 import { ref } from 'vue'
+let modal = ref(false)
 const chamados = useRegistrosStore()
 const showEditor = ref(false)
 const editedChamado = ref({}) // Objeto para armazenar os dados do chamado editado
@@ -30,8 +31,13 @@ function fecharEditor() {
   editedChamado.value = {}
 }
 
+function openModal(){
+    modal.value = true
+}
 
-
+function doneModal(){
+    modal.value = false 
+}
 
 </script>
 
@@ -46,7 +52,7 @@ function fecharEditor() {
 
                 <button v-if="!chamado.concluido" @click="chamados.removerChamado(index)" class="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold hover:bg-red-700 dark:text-white dark:border-gray-300 border-gray-500 text-black border rounded-md top-1 right-9 "><span class="material-symbols-outlined text-sm">delete</span></button>
 
-                <button v-if="!chamado.concluido" @click="abrirEditor(index)" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold hover:bg-blue-700 dark:text-white dark:border-gray-300 border-gray-500 text-black border rounded-md top-1 right-16 "><span class="material-symbols-outlined text-sm">edit</span></button>
+                <button v-if="!chamado.concluido" @click="abrirEditor(index), openModal()"  class="absolute inline-flex items-center justify-center w-5 h-5 text-xs font-bold hover:bg-blue-700 dark:text-white dark:border-gray-300 border-gray-500 text-black border rounded-md top-1 right-16 "><span class="material-symbols-outlined text-sm">edit</span></button>
 
                 <div class="flex items-center justify-center gap-3 text-lg font-semibold mt-3">
                     <div v-if="chamado.checkTipo == 'requisicao'">
@@ -77,15 +83,12 @@ function fecharEditor() {
 
 
 <!-- Main modal -->
-<div  id="authentication-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div v-if="modal == true" class="fixed top-0 left-0 z-50 right-0 flex items-center justify-center bg-opacity-60 bg-gray-950 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full">
     <div class="relative w-full max-w-md max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
-                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                </svg>
-                <span @click="cancelarEdicao" class="sr-only">Close modal</span>
+                <span @click="cancelarEdicao, doneModal()"><span class="material-symbols-outlined">close</span></span>
             </button>
             <div  class="px-6 py-6 lg:px-8">
                 <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Editar chamados</h3>
